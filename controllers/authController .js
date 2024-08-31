62d0af52fa212eb95d427b9e74233a168f49c13e
mustkim <codesiddhant@gmail.com>
2024-08-31 14:52:16 +0530

    existing user check added

--- controllers/authController .js
+++ controllers/authController .js
@@ -3,6 +3,7 @@ const userModel = require("../models/userModel");
 const registerController = async (req,res)=>{
     try {
         const { username, email, password,address, phone } = req.body;
+
         //validation
         if(!username || !email || !password || !address || !phone){
             return res.status(500).send({
@@ -11,21 +12,23 @@ const registerController = async (req,res)=>{
             });
         }
 
+
+        // create new user
+        const user = await userModel.create({username, email, password, address, phone});
+         res.status(201).send({
+            success:true,
+            message:"Successfully Registered",
+            user 
+        });
+
         // check existing user
-        const existingUser = await User.findOne({email});
+        const existingUser = await userModel.find({email});
         if(existingUser){
             res.status(500).send({
                 success:false,
                 message:"User already exists"
             });
-        }
-
-        // create new user
-        const user = await userModel.create({username, email, password, address, phone});
-        res.status(201).send({
-            success:true,
-            message:"Successfully Registered",
-        });
+        };
 
     } catch (error) {
         console.log(error);
