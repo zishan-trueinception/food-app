1c1af84f6c837815465d8743a7b943a9c5247f20
Zishan Shaikh <zishan@truelink.ai/in>
2024-09-03 14:36:36 +0530

    update api created

--- controllers/userController.js
+++ controllers/userController.js
@@ -31,4 +31,36 @@ const getUserController = async (req,res) => {
     }
 }
 
-module.exports = { getUserController }
+// UPDATE USER
+const updateUserController = async (req,res) => {
+    try {
+        // find user
+        const user = await userModel.findById({_id:req.body.id});
+        // validation
+        if(!user){
+            return res.status(404).send({
+                success:false,
+                message:"User not found"
+            })
+        }
+        // update user
+        const {username,address,phone} = req.body
+        if(username)user.username = username
+        if(address)user.address = address
+        if(phone)user.phone = phone
+        // save user
+        await user.save()
+        res.status(200).send({
+            success:true,
+            message:"User Updated Successfully",
+        })
+    } catch (error) {
+        res.status(500).send({
+            success:"false",
+            message:"Error in updating user",
+            error
+        })
+    }
+}
+
+module.exports = { getUserController, updateUserController}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,13 @@
 const expres = require('express');
-const { getUserController } = require('../controllers/userController');
+const { getUserController, updateUserController } = require('../controllers/userController');
 const { authMiddleware } = require('../middlewares/authMiddleware');
 const router = expres.Router();
 
 //routes
 // GET USER | GET
 router.get('/getUser',authMiddleware,getUserController)
+
+// UPDATE PROFILE | PUT
+router.put('/updateUser',authMiddleware,updateUserController)
 
 module.exports = router
