4ef1f1f8b9e6f72968eb3131f8a9fc1da4b54bda
Zishan Shaikh <zishan@truelink.ai/in>
2024-09-04 16:22:19 +0530

    CATEGORY CURD OPERATION COMPLETED

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,67 +1,121 @@
 const categoryModel = require("../models/categoryModel");
 const  route  = require("../routes/categoryRoute");
 
-
-const getcatergorycontroller = async (req,res)=>{
+// create category controller route model
+const createCatergoryController = async (req,res)=>{
     try {
-        const catergory = await categoryModel.find();
-        try {
-            res.status(200).send({
-                success:"got",
-                catergory
-            })
-        } catch (error) {
-            console.log(error);
-            res.status(500).send({
+        const { title, imageUrl } = req.body
+        // validation
+        if(!title){
+            return res.status(500).send({
                 success:false,
-                message:"Error while getting catergory",
-                error
+                message:"Please Provide title"
             })
         }
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({
-            success:false,
-            message:"Error while getting catergory",
-            error
-        })
-    }
-};
-
-const postcatergorycontroller = async (req,res)=>{
-    try {
-        const { categoryName, name, img, options, discription } = req.body;
-        
-        const catergory = await categoryModel.create({
-            categoryName,
-            name,
-            img,
-            options,
-            discription
+        // create new category
+        const newCategory = new categoryModel({ title, imageUrl })
+        await newCategory.save();
+        res.status(200).send({
+            success:true,
+            message:"Category Created Successfully",
+            newCategory
         });
-        try {
-            res.status(200).send({
-                success:"got",
-                catergory
-            })
-        } catch (error) {
-            console.log(error);
-            res.status(500).send({
+    } catch (error) {
+        res.status(500).send({
+            success:false,
+            message:"Error while creating catergory",
+            error
+        })
+    }
+}
+
+// GET ALL CATEGORY ROUTE | GET
+const getAllCatController = async (req,res)=>{
+    try {
+        const categories = await categoryModel.find({})
+        // validation
+        if(!categories){
+            return res.status(404).send({
                 success:false,
-                message:"Error while getting catergory",
-                error
+                message:"No category found"
             })
         }
+        res.status(200).send({
+            success:true,
+            totalCategories:categories.length,
+            message:"Categories found successfully",
+            categories
+        })
+    } catch (error) {
+        res.status(500).send({
+            success:true,
+            message:"Error in Get Category API",
+            error
+        })
+    }
+}
+
+// UPDATE CATEGORY ROUTE | PUT
+const updateCatController = async (req,res)=>{
+    try {
+        const {id} = req.params
+        const {title,imageUrl} = req.body
+        const updatedCategory = await categoryModel.findByIdAndUpdate(id,{title,imageUrl}, {new:true})
+        if(!updatedCategory){
+            return res.status(404).send({
+                success:false,
+                message:"No category found with this Id"
+            })
+        }
+        res.status(200).send({
+            success:true,
+            message:"Category Updated Successfully"
+        })
     } catch (error) {
         console.log(error);
         res.status(500).send({
             success:false,
-            message:"Error while getting catergory",
+            message:"Error in updating category",
             error
         })
     }
-};
+}
+
+// DELETE CATEGORY ROUTE | DELETE
+const deleteCatController = async (req,res)=>{
+    try {
+        const {id} = req.params;
+        if(!id){
+            return res.status(404).send({
+                success:false,
+                message:"Id is required"
+            })
+        }
+        const category = await categoryModel.find({id})
+        if(!category){
+            return res.status(404).send({
+                success:false,
+                message:"Category Not Found"
+            })
+        }
+        await categoryModel.findByIdAndDelete(id);
+        res.status(200).send({
+            success:true,
+            message:"Category Deleted Successfully"
+        })
+    } catch (error) {
+        res.status(500).send({
+            success:false,
+            message:"Error in Deleting Category",
+        })
+    }
+}
 
 
+module.exports = {
+    createCatergoryController,
+    getAllCatController,
+    updateCatController,
+    deleteCatController
 
-module.exports = getcatergorycontroller
+}
